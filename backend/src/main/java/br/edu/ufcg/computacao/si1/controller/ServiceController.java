94cf34d00d114efe284da0c8f7630270db98f222
Lucas Silva <lucas.silva@ccc.ufcg.edu.br>
2017-03-28 21:16:18 -0300

    Added missing throws to method

--- backend/src/main/java/br/edu/ufcg/computacao/si1/controller/ServiceController.java
+++ backend/src/main/java/br/edu/ufcg/computacao/si1/controller/ServiceController.java
@@ -1,20 +1,27 @@
 package br.edu.ufcg.computacao.si1.controller;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.ufcg.computacao.si1.exceptions.BuyOwnAdvertisementException;
 import br.edu.ufcg.computacao.si1.exceptions.InsufficientCreditException;
 import br.edu.ufcg.computacao.si1.exceptions.PurchaseNotServiceException;
 import br.edu.ufcg.computacao.si1.exceptions.UserNotFoundException;
 import br.edu.ufcg.computacao.si1.models.user.User;
 import br.edu.ufcg.computacao.si1.services.AuthenticationService;
 import br.edu.ufcg.computacao.si1.services.NegotiationService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.Optional;
 
 /**
  * Created by gustavo on 23/03/17.
@@ -52,7 +59,7 @@ public class ServiceController {
             return new ResponseEntity<>(HttpStatus.OK);
         } catch (UserNotFoundException e) {
             return new ResponseEntity<>(HttpStatus.FORBIDDEN);
-        } catch (InsufficientCreditException e) {
+        } catch (InsufficientCreditException | BuyOwnAdvertisementException e) {
             return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
         } catch (PurchaseNotServiceException e) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
